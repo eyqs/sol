2b5fa867fd4853e507e1bb4f86c6e09cd697123f
eyqs <eugeneyqshen@gmail.com>
2016-05-18 21:17:11 -0700

    Restart Sudoku with better foundation

--- sol.rkt
+++ sol.rkt
@@ -14,10 +14,11 @@
 (define BOX-HEIGHT (* 3 CELL-HEIGHT))
 (define BOARD-WIDTH (+ (* 3 BOX-WIDTH) (* 2 LINE-WIDTH)))
 (define BOARD-HEIGHT (+ (* 3 BOX-HEIGHT) (* 2 LINE-HEIGHT)))
+(define EMPTY-COLOUR "White")
 (define SLICE-COLOUR "Moccasin")
 (define FOCUS-COLOUR "LightSalmon")
 (define SPACE-COLOUR "LightCyan")
-(define NEW-COLOUR "LightGreen")
+(define PLACE-COLOUR "LightGreen")
 
 
 
@@ -25,10 +26,19 @@
 ;; Windowing:
 
 (define frame (new frame% [label "sol"]))
+(define menu-bar (new menu-bar% [parent frame]))
+(define menu (new menu% [label "&Settings"] [parent menu-bar]))
+(new menu-item% [parent menu] [label "Quit"]
+     [callback (lambda (menu-item event) (exit))])
 (define canvas (new canvas% [parent frame] [style '(transparent)]
                     [min-width BOARD-WIDTH] [min-height BOARD-HEIGHT]))
-(send frame show true)
 (define DC (send canvas get-dc))
+(define panel (new horizontal-panel% [parent frame] [alignment '(center center)]))
+(new button% [parent panel] [label "<<"]
+     [callback (lambda (button event) (prev))])
+(new button% [parent panel] [label ">>"]
+     [callback (lambda (button event) (next))])
+(send frame show true)
 
 
 
@@ -54,16 +64,16 @@
 ;; interp. (make-cell value colour) is a cell, where
 ;;         value is the value of the cell, as a Value
 ;;         colour is the colour of the cell, as a String
-(define C0 (make-cell V0 "White"))
-(define C1 (make-cell V1 "White"))
-(define C2 (make-cell V2 "White"))
-(define C3 (make-cell V3 "White"))
-(define C4 (make-cell V4 "White"))
-(define C5 (make-cell V5 "White"))
-(define C6 (make-cell V6 "Moccasin"))
-(define C7 (make-cell V7 "LightSalmon"))
-(define C8 (make-cell V8 "LightCyan"))
-(define C9 (make-cell V9 "LightGreen"))
+(define C0 (make-cell V0 EMPTY-COLOUR))
+(define C1 (make-cell V1 EMPTY-COLOUR))
+(define C2 (make-cell V2 EMPTY-COLOUR))
+(define C3 (make-cell V3 EMPTY-COLOUR))
+(define C4 (make-cell V4 EMPTY-COLOUR))
+(define C5 (make-cell V5 EMPTY-COLOUR))
+(define C6 (make-cell V6 SLICE-COLOUR))
+(define C7 (make-cell V7 FOCUS-COLOUR))
+(define C8 (make-cell V8 SPACE-COLOUR))
+(define C9 (make-cell V9 PLACE-COLOUR))
 
 ;; Board is (listof Cell)
 ;; interp. a list of all 81 cells in the board
@@ -117,6 +127,13 @@
 ;; =================
 ;; Data conversions:
 
+;; Value -> String
+;; convert Value to String
+(define (val->str v)
+  (if (number? v)
+      (number->string v)
+      ""))
+
 ;; Position -> Natural[0, 9)
 ;; convert Position to zero-indexed row and column
 (define (pos->row p)
@@ -129,16 +146,16 @@
 (define (rc->pos r c)
   (+ (* r 9) c))
 
-;; Board Position -> Value
+;; Board Position -> Cell
 ;; produce the value at the given position on the board
-(define (read-square b p)
+(define (read-cell b p)
   (list-ref b p))
 
-;; Board Position Value -> Board
-;; produce a new board with the given value at the given position
-(define (fill-square b p v)
+;; Board Position Cell -> Board
+;; produce a new board with the given cell at the given position
+(define (fill-cell b p c)
   (append (take b p)
-          (list v)
+          (list c)
           (drop b (add1 p))))
 
 
@@ -146,248 +163,43 @@
 ;; =================
 ;; Functions:
 
-;; Board -> Board or false
-;; produce a solution for bd, or false if bd is unsolvable
-;; ASSUME: bd is valid
-(define (solve bd)
-  (local [(define (fn-for-bd bd)
-            (if (solved? bd)
-                bd
-                (fn-for-lobd (next-boards bd))))
-          (define (fn-for-lobd lobd)
-            (cond [(empty? lobd) false]
-                  [else
-                   (local [(define try (fn-for-bd (first lobd)))]
-                     (if (not (false? try))
-                         try
-                         (fn-for-lobd (rest lobd))))]))]
-    (fn-for-bd bd)))
+;; world is the list of all intermediate boards
+;; generated in the process of solving a Sudoku
+;; space is the index of the current board in world
+(define world (list B1))
+(define space 0)
 
-;; Board -> Boolean
-;; produce true if board is solved
-;; ASSUME: board is valid, so it is solved if it is full
-(define (solved? bd)
-  (andmap number? bd))
+;; start with a new board
+(define (main b)
+  (begin (set! world (list b))
+         (set! space 0)
+         (render)))
 
-;; Board -> (listof Board)
-;; produce list of valid next boards from board
-;; find first empty square, fill it with Natural[1, 9], keep only valid boards
-(define (next-boards bd)
-  (keep-only-valid (fill-with-1-9 (find-blank bd) bd)))
-
-;; Board -> Pos
-;; produce the position of the first blank square
-;; ASSUME: the board has at least one blank square
-(define (find-blank bd)
-  (cond [(empty? bd) (error "The board didn't have a blank space.")]
-        [else
-         (if (false? (first bd))
-             0
-             (+ 1 (find-blank (rest bd))))]))
-
-;; Pos Board -> (listof Board)
-;; produce 9 boards, with blank filled with Natural[1, 9]
-(define (fill-with-1-9 p bd)
-  (local [(define (build-one n)
-            (fill-square bd p (+ n 1)))]
-    (build-list 9 build-one)))
-
-;; (listof Board) -> (listof Board)
-;; produce list containing only valid boards
-(define (keep-only-valid lobd)
-  (filter valid-board? lobd))
-
-;; Board -> Boolean
-;; produce true if no unit on the board has the same natural twice, false otherwise
-(define (valid-board? bd)
-  (local [(define (valid-units? lou)        ;(listOf Unit) -> Boolean
-            (andmap valid-unit? lou))       ; - produce true if all units are valid
-          (define (valid-unit? lop)         ;(listof Pos) -> Boolean
-            (no-duplicates?                 ; - produce true if a single unit is valid
-             (keep-only-values
-              (read-unit lop))))
-          (define (no-duplicates? lon)      ;(listof Natural[1, 9]) -> Boolean
-            (cond [(empty? lon) true]       ; - produce true if no natural appears twice
-                  [else
-                   (if (member (first lon) (rest lon))
-                       false
-                       (no-duplicates? (rest lon)))]))
-          (define (keep-only-values lov)    ;(listof Val) -> (listof Natural[1, 9])
-            (filter number? lov))           ; - produce a list of only naturals
-          (define (read-unit lop)           ;(listof Pos) -> (listof Val)
-            (map read-pos lop))             ; - produce values of bd in the unit
-          (define (read-pos p)              ;Pos -> Val
-            (read-square bd p))]            ; - produce contents of bd at p
-    (valid-units? UNITS)))
-
-;; Board -> Image
-;; render an image of the current board
-(define (render bd)
-  (local [(define (render-cell pos val)
-            (local [(define x (+ (* CELL-WIDTH (pos->col pos))
-                                 (* LINE-WIDTH (quotient (pos->col pos) 3))))
-                    (define y (+ (* CELL-HEIGHT (pos->row pos))
-                                 (* LINE-HEIGHT (quotient (pos->row pos) 3))))
-                    (define (val->string val)
-                      (if (number? val)
-                          (number->string val)
-                          ""))
-                    (define-values (w h d a) (send DC get-text-extent (val->string val)))]
-              (send DC draw-rectangle x y CELL-WIDTH CELL-HEIGHT)
-              (send DC draw-text (val->string val)
-                    (+ x (/ (- CELL-WIDTH w) 2)) (+ y (/ (- CELL-HEIGHT h) 2 )))))]
-    (for ([i bd] [j (build-list 81 identity)])
-      (render-cell j i))))
-
-;; Board String (listof Pos) Image -> Image
-;; highlight a list of positions on the board with the given colour
-(define (highlight bd col lop img)
-  (local [(define (highlight-all bd col lop img)
-            (cond [(empty? lop) img]
-                  [else
-                   (highlight-one bd col (first lop) (highlight-all bd col (rest lop) img))]))
-          (define (highlight-one bd col p img)
-            (pin-over img
-                      (x-coordinate (pos->col p))
-                      (y-coordinate (pos->row p))
-                      (cc-superimpose
-                       (filled-rectangle CELL-WIDTH CELL-HEIGHT #:color col)
-                       (number-image bd p))))
-          (define (x-coordinate n)
-            (+ (* n CELL-WIDTH)
-               (* (quotient n 3) LINE-WIDTH)))
-          (define (y-coordinate n)
-            (+ (* n CELL-HEIGHT)
-               (* (quotient n 3) LINE-HEIGHT)))
-          (define (number-image bd p)
-            (if (number? (read-square bd p))
-                (text (number->string (read-square bd p)) null FONT-SIZE 0)
-                (blank)))]
-    (highlight-all bd col lop img)))
-
-;; Temporary testing function before I make this into a world program
-(define-struct testboard (bd box n))
-(define (test-spaces bd)
-  (local [(define (test-spaces bd unit)
-            (cond [(<= 27 unit) (test-slices bd)]
-                  [(equal? bd (solve-spaces bd (list-ref UNITS unit)))
-                   (test-spaces bd (add1 unit))]
-                  [else
-                   (make-testboard (solve-spaces bd (list-ref UNITS unit)) unit false)]))]
-    (test-spaces bd 0)))
-(define (test-slices bd)
-  (local [(define (test-slices bd box n)
-            (cond [(< 9 n) (make-testboard bd false false)]
-                  [(= 9 box) (test-slices bd 0 (add1 n))]
-                  [(equal? bd (solve-slices bd (list-ref BOXES box) n))
-                   (test-slices bd (add1 box) n)]
-                  [else
-                   (make-testboard (solve-slices bd (list-ref BOXES box) n) box n)]))]
-    (test-slices bd 0 1)))
-(define B false)
-(define test-bd
-  (list 1 3 B 2 B B 7 4 B
-        B 2 5 B 1 B B B B
-        4 8 B B 6 B B 5 B
-        B B B 7 8 B 2 1 B
-        5 B B B 9 B 3 7 B
-        9 B B B 3 B B B 5
-        B 4 B B B 6 8 9 B
-        B 5 3 B B 1 4 B B
-        6 B B B B B B B B))
-(define tb (make-testboard test-bd false false))
+;; move to the next board
 (define (next)
-  (begin
-    (set! test-bd (testboard-bd tb))
-    (set! tb (test-spaces test-bd))
-    (if (not (false? (testboard-box tb)))
-        (if (not (false? (testboard-n tb)))
-            (highlight-slices test-bd (list-ref BOXES (testboard-box tb)) (testboard-n tb) (render test-bd))
-            (highlight test-bd SPACE-COLOUR (list-ref UNITS (testboard-box tb)) (render test-bd)))
-        (render test-bd))))
+  (if (zero? (- (length world) space 1))
+      (render)
+      (begin (set! space (add1 space))
+             (render))))
 
-;; Board Unit Natural[1, 9] -> Board
-;; given a board, a box unit, and a number, produce the board with the number
-;; filled in inside the box unit if there is only one space for it to be
-(define (solve-slices bd box n)
-  (if (in-unit? bd box n)
-      bd
-      (local [(define (solve-slices bd box n acc)
-                (if (empty? box)
-                    (if (empty? acc)
-                        bd
-                        (if (empty? (rest acc))
-                            (fill-square bd (first acc) n)
-                            bd))
-                    (local [(define column (list-ref COLS (pos->col (first box))))
-                            (define row (list-ref ROWS (pos->row (first box))))
-                            (define colpos (in-unit? bd column n))
-                            (define rowpos (in-unit? bd row n))]
-                      (cond [(and (false? colpos) (false? rowpos) (false? (read-square bd (first box))))
-                             (solve-slices bd (rest box) n (cons (first box) acc))]
-                            [else
-                             (solve-slices bd (rest box) n acc)]))))]
-        (solve-slices bd box n empty))))
+;; move to the previous board
+(define (prev)
+  (if (zero? space)
+      (render)
+      (begin (set! space (sub1 space))
+             (render))))
 
-;; Board Unit Natural[1, 9] Image -> Image
-;; given a board, a box unit, a number, and an image, produce a board image that highlights
-;; every row and column that intersects the box, if the number is in that row or column
-(define (highlight-slices bd box n img)
-  (if (empty? box)
-      img
-      (local [(define column (list-ref COLS (pos->col (first box))))
-              (define row (list-ref ROWS (pos->row (first box))))
-              (define colpos (in-unit? bd column n))
-              (define rowpos (in-unit? bd row n))]
-        (cond [(not (false? colpos))
-               (if (not (false? rowpos))
-                   (highlight bd FOCUS-COLOUR (list colpos)
-                              (highlight bd SLICE-COLOUR column
-                                         (highlight bd FOCUS-COLOUR (list rowpos)
-                                                    (highlight bd SLICE-COLOUR row
-                                                               (highlight-slices bd (rest box) n img)))))
-                   (highlight bd FOCUS-COLOUR (list colpos)
-                              (highlight bd SLICE-COLOUR column
-                                         (highlight-slices bd (rest box) n img))))]
-              [(not (false? rowpos))
-               (highlight bd FOCUS-COLOUR (list rowpos)
-                          (highlight bd SLICE-COLOUR row
-                                     (highlight-slices bd (rest box) n img)))]
-              [else
-               (highlight-slices bd (rest box) n img)]))))
-
-;; Board Unit Natural[1, 9] -> Pos or false
-;; given a board, a unit and a number, return the position
-;; of the number in the unit or false if it is not there
-(define (in-unit? bd lop n)
-  (cond [(empty? lop) false]
-        [(and (number? (read-square bd (first lop)))
-              (= n (read-square bd (first lop))))
-         (first lop)]
-        [else
-         (in-unit? bd (rest lop) n)]))
-
-;; Board Unit -> Board
-;; given a board and a unit, produce the board with the unit
-;; filled in if there is only one number left to fill
-(define (solve-spaces bd lop)
-  (local [(define (solve-spaces lon)
-            (if (= 8 (length values))
-                (fill-square bd
-                             (get-pos lop)
-                             (get-value lon))
-                bd))
-          (define (get-value lon)
-            (first (remove* values lon)))
-          (define (get-pos lop)
-            (if (false? (read-pos (first lop)))
-                (first lop)
-                (get-pos (rest lop))))
-          (define (keep-only-values lov)
-            (filter number? lov))
-          (define (read-unit lop)
-            (map read-pos lop))
-          (define (read-pos p)
-            (read-square bd p))
-          (define values (keep-only-values (read-unit lop)))]
-    (solve-spaces '(1 2 3 4 5 6 7 8 9))))
+;; render an image of the current board
+(define (render)
+  (local [(define (render-cell p c)
+            (local [(define x (+ (* CELL-WIDTH (pos->col p))
+                                 (* LINE-WIDTH (quotient (pos->col p) 3))))
+                    (define y (+ (* CELL-HEIGHT (pos->row p))
+                                 (* LINE-HEIGHT (quotient (pos->row p) 3))))
+                    (define-values (w h d a) (send DC get-text-extent (val->str (cell-value c))))]
+              (send DC set-brush (new brush% [color (cell-colour c)]))
+              (send DC draw-rectangle x y CELL-WIDTH CELL-HEIGHT)
+              (send DC draw-text (val->str (cell-value c))
+                    (+ x (/ (- CELL-WIDTH w) 2)) (+ y (/ (- CELL-HEIGHT h) 2 )))))]
+    (for ([i (build-list 81 identity)] [j (list-ref world space)])
+      (render-cell i j))))
